ae5db0cf0d28139860653e001f8ba053511f5dc8
M-AmirLatif <amirlatif2288@gmail.com>
2025-12-18 16:31:09 -0800

    Remove node_modules and old Assignment1 files

--- node/node1.js
+++ node/node1.js
@@ -1,1 +1,28 @@
-console.log("hello");
+var pdf = require('pdf-creator-node')
+var fs = require('fs')
+
+// load HTML template
+var html = fs.readFileSync('template.html', 'utf8')
+
+var options = {
+  format: 'A3',
+  orientation: 'portrait',
+  border: '10mm',
+}
+
+var users = [
+  { name: 'Shyam', age: 26 },
+  { name: 'Navjot', age: 26 },
+  { name: 'Vitthal', age: 26 },
+]
+
+var document = {
+  html: html,
+  data: { users: users },
+  path: './output.pdf',
+}
+
+pdf
+  .create(document, options)
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err))
